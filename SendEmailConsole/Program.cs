5313570b949111bcb4519176145cc96b9c5e7a6c
Ransom Cadorette <91761633+RansomDC@users.noreply.github.com>
2022-10-16 22:36:51 -0700

    pre-merge commit

--- SendEmailConsole/Program.cs
+++ SendEmailConsole/Program.cs
@@ -13,6 +13,7 @@ namespace SendEmailConsole
         public static void Main(string[] args)
         {
 
+
             //CreateDefaultBuilder() Creates a host object which loads IConfiguration for appsettings.json
             //configureservices enables the use of dependency injection using the services that are declared within the method call
             var host = Host.CreateDefaultBuilder()
@@ -25,8 +26,9 @@ namespace SendEmailConsole
             // This creates an Email object instance using the interface from the service declared above
             var emailSVC = ActivatorUtilities.CreateInstance<Email>(host.Services);
 
-            
 
+
+            //User Interface for sending emails
             bool writeAnother = true;
             while (writeAnother == true)
             {
@@ -39,44 +41,86 @@ namespace SendEmailConsole
 
                 Console.WriteLine("Please enter the email's subject");
                 string subject = Console.ReadLine();
+                if (subject == "") { subject = "<No Subject>"; }
 
                 Console.WriteLine("Please enter the message you would like to send");
                 string body = Console.ReadLine();
+                if (body == "") { body = "<No body>"; }
 
                 Console.WriteLine("Sending your email!");
 
-
-
-
-                // This calls the Email instance's SendEmail() method asynchronously
-                _ = emailSVC.SendEmail(sender, recipient, subject, body);
-
-                Console.WriteLine("Would you like to write another email? Y/N");
-                string response = Console.ReadLine();
-                if (response != "Y" && response != "N")
+                for(int i = 1; i < 3; i++)
                 {
-                    Console.WriteLine("Please enter only Y or N");
+                    try
+                    {
+                        // This calls the Email instance's SendEmail() method asynchronously
+                        _ = emailSVC.SendEmail(sender, recipient, subject, body);
+                        _ = writeToDB(sender, recipient, subject, body, true);
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+
+                        if(i == 3)
+                        {
+                            _ = writeToDB(sender, recipient, subject, body, false);
+                            Console.WriteLine(ex);
+                            Console.WriteLine("Your email failed to send after 3 retries. A log has been made. Please contact your system administrator if the problem continues.");
+                        }
+                    }
                 }
-                else if(response == "N")
+
+                //Get user response of whether to continue writing emails or to exit program.
+                while(writeAnother == true)
                 {
-                    writeAnother = false;
+                    Console.WriteLine("Would you like to write another email? Y/N");
+                    string response = Console.ReadLine();
+                    if (response != "Y" && response != "N")
+                    {
+                        Console.WriteLine("Please enter only Y or N");
+                    }
+                    else if (response == "N")
+                    {
+                        writeAnother = false;
+                    }
+                    else
+                    {
+                        break;
+                    }
                 }
+                
 
             }
 
+            // A method to write the email data to the database asynchronously.
+            static async Task writeToDB(string sender, string recipient, string subject, string body, bool sendSuccessful)
+            {
 
+                using (var context = new EmailContext())
+                {
+                    var em = new EmailEntity()
+                    {
+                        senderAddress = sender,
+                        recipeintAddress = recipient,
+                        subject = subject,
+                        sendSuccessful = sendSuccessful,
+                        sendTime = DateTime.Now,
+                        body = body
+                    };
 
+                    context.Emails.Add(em);
+                    await context.SaveChangesAsync();
+                }
+            }
 
-
-
-
-
-            Console.ReadLine();
         }
 
 
         //Added NuGet package:   Microsoft.Extensions.Hosting
         //Added NuGet package:   Microsoft.EntityFrameworkCore.SqlServer
+        //Added NuGet package:   Microsoft.EntityFrameworkCore.Design
+        //Added NuGet package:   Microsoft.EntityFrameworkCore
+        //Added NuGet package:   
 
 
     }
